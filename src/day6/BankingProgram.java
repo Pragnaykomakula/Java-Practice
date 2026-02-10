7ed0b949ece7a5ad8439bf65261442fe77777921
Pragnay Komakula <komakulapragnay@gmail.com>
2026-02-10 22:10:01 +0530

    Day6

---
+++ src/day6/BankingProgram.java
@@ -0,0 +1,81 @@
+package day6;
+
+import java.util.Scanner;
+
+public class BankingProgram {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+
+
+        double balance = 0;
+        boolean isRunning = true;
+        int choice;
+
+        while(isRunning){
+            System.out.println("--------------------------------");
+            System.out.println("WELCOME TO BANK PROGRAMMING");
+            System.out.println("--------------------------------");
+            System.out.println("1. SHOW BALANCE");
+            System.out.println("2. DEPOSIT");
+            System.out.println("3. WITHDRAW");
+            System.out.println("4. EXIT");
+            System.out.println("--------------------------------");
+
+            System.out.print("Enter your choice(1-4): ");
+            choice = sc.nextInt();
+            System.out.println("--------------------------------");
+
+
+            switch(choice){
+                case 1 -> showBalance(balance);
+                case 2 -> balance += deposit();
+                case 3 -> balance = balance - withdraw(balance);
+                case 4 -> isRunning = false;
+                default -> System.out.println("Invalid choice,Please choose between (1 - 4)");
+            }
+
+            System.out.println("Thankyou have a nice day");
+            System.out.println("--------------------------------");
+
+
+        }
+
+        }
+        static void showBalance(double balance){
+            System.out.printf("$%.2f \n",balance);
+        }
+        static double deposit(){
+
+        double amount;
+
+        System.out.print("Enter an amount to deposited: ");
+        amount = sc.nextDouble();
+
+        if(amount < 0){
+            System.out.println("Negative Number are Invalid");
+            return 0;
+        }else{
+            return amount;
+        }
+
+    }
+        static double withdraw(double balance){
+        double amount;
+
+            System.out.print("Enter an amount to withdraw: ");
+            amount = sc.nextDouble();
+
+            if(amount > balance){
+                System.out.println("Insufficient Balance");
+                return 0;
+            }else if(amount < 0){
+                System.out.println("Amount cant be negative");
+                return 0;
+            }else{
+                return amount;
+            }
+
+
+        }
+}
+
---
+++ src/day6/DiceRoller.java
@@ -0,0 +1,96 @@
+package day6;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class DiceRoller {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random rc = new Random();
+
+        int numOfDice;
+        int total = 0;
+
+        System.out.print("Enter a number of dice to roll: ");
+        numOfDice = sc.nextInt();
+
+       if(numOfDice > 0){
+           for(int i = 0;i < numOfDice ; i++){
+               int roll = rc.nextInt(1,7);
+               printDie(roll);
+               System.out.println("you have rolled: "+ roll);
+               System.out.println();
+               total += roll;
+
+           }
+           System.out.print("Total num of roll: "+ total);
+       }else{
+           System.out.println("Invalid number");
+       }
+
+
+
+
+    }
+    static void printDie(int roll){
+        String die1 = """
+                  -----
+                |       |
+                |   ●   |
+                |       |
+                  -----
+                """;
+        String die2 = """
+                  -----
+                | ●     |
+                |       |
+                |     ● |
+                  -----
+                """;
+
+        String die3 = """
+                  -----
+                | ●     |
+                |   ●   |
+                |     ● |
+                  -----
+                """;
+
+        String die4 = """
+                  -----
+                | ●   ● |
+                |       |
+                | ●   ● |
+                  -----
+                """;
+
+        String die5= """
+                  -----
+                | ●   ● |
+                |   ●   |
+                | ●   ● |
+                  -----
+                """;
+
+        String die6= """
+                  -----
+                | ●   ● |
+                | ●   ● |
+                | ●   ● |
+                  -----
+                """;
+        switch (roll){
+            case 1 -> System.out.println((die1));
+            case 2 -> System.out.println((die2));
+            case 3 -> System.out.println((die3));
+            case 4 -> System.out.println((die4));
+            case 5 -> System.out.println((die5));
+            case 6 -> System.out.println((die6));
+            default -> System.out.println("Invalid roll");
+
+
+        }
+
+    }
+
+}
